475655155de756ba35e7a01e0a8fa399269f3488
“Nuha” <nuha14m@gmail.com>
2021-07-26 17:43:55 +0000

    Redirect on login.

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,4 +1,13 @@
 const singInButton = document.querySelector(".button")
 singInButton .addEventListener("click", ()=>{
     console.log("clicked!")
+    const provider = new firebase.auth.GoogleAuthProvider()
+    firebase.auth().signInWithPopup(provider).then(result =>{
+        const user = result.user
+        console.log("login success", user)
+        window.location = "writeNote.html"
+    })
+    .catch(error =>{
+        console.log("login failed", error)
+    })
 })
